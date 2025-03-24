ef70b4019273eafd2d0f8937e2dc59529e797848
Lucas Silva <lucasvida2005@gmail.com>
2025-03-24 11:23:26 -0300

    Improving code

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String appTitle = "Flutter layout demo";
-    Color scaffoldColor = Colors.blueGrey[50]!;
-    Color containerColor = Colors.white;
-    Color appBarColor = Colors.indigo[700]!;
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -20,35 +17,39 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text(appTitle, style: TextStyle(color: Colors.white)),
           centerTitle: true,
-          backgroundColor: appBarColor,
+          backgroundColor: Colors.indigo[700],
         ),
-        backgroundColor: scaffoldColor,
-        body: Center(
-          child: Container(
-            color: containerColor,
-            padding: EdgeInsets.all(16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(8),
-                  child: Text("Row of buttons", style: TextStyle(fontSize: 20)),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    OutlinedButton(onPressed: () {}, child: Text("Botao 01")),
-                    SizedBox(width: 12),
-                    OutlinedButton(onPressed: () {}, child: Text("Botao 02")),
-                    SizedBox(width: 12),
-                    OutlinedButton(onPressed: () {}, child: Text("Botao 03")),
-                  ],
-                ),
-              ],
-            ),
+        body: Center(child: ButtonSection()),
+      ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: Text("Row of buttons", style: TextStyle(fontSize: 20)),
           ),
-        ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              OutlinedButton(onPressed: () {}, child: Text("Botao 01")),
+              SizedBox(width: 12),
+              OutlinedButton(onPressed: () {}, child: Text("Botao 02")),
+              SizedBox(width: 12),
+              OutlinedButton(onPressed: () {}, child: Text("Botao 03")),
+            ],
+          ),
+        ],
       ),
     );
   }
